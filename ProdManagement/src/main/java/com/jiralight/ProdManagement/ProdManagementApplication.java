f4b550146525d5ddf2864558f4e7b69ecb681d45
Ankit Gaur <iamankitgaur@gmail.com>
2024-05-05 14:50:27 +0530

    Initial Commit Added project structure

---
+++ ProdManagement/src/main/java/com/jiralight/ProdManagement/ProdManagementApplication.java
@@ -0,0 +1,13 @@
+package com.jiralight.ProdManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdManagementApplication.class, args);
+	}
+
+}
---
+++ ProdManagement/src/test/java/com/jiralight/ProdManagement/ProdManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiralight.ProdManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
